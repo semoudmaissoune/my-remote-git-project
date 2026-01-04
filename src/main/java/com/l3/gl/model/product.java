4fa732bdf129a70cc78e43b48477eb85ffad2bdc
salwa <soussa.btj00@gmail.com>
2026-01-04 22:58:25 +0100

    Add User and Product model classes

---
+++ src/main/java/com/l3/gl/model/product.java
@@ -0,0 +1,10 @@
+package com.l3.gl.model;
+public class product {
+private String name;
+private double price;
+public product(String name, double price) {
+this.name = name;
+this.price = price;
+}
+// Getters and setters
+}
