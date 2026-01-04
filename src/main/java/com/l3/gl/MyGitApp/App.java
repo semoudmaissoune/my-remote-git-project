b24799d894cd74b256d18be8d5f889a8796b723a
salwa <soussa.btj00@gmail.com>
2026-01-04 22:32:27 +0100

    Initial commit: Setup Maven project structure.

---
+++ src/main/java/com/l3/gl/MyGitApp/App.java
@@ -0,0 +1,13 @@
+package com.l3.gl.MyGitApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
